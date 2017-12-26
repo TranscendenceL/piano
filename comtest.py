39e78bb6eba0a45ff794cc23ae2cfb32a4a8cf6b
TranscendenceL <32671207+TranscendenceL@users.noreply.github.com>
2017-12-26 10:34:10 +0800

    amendment

--- comtest.py
+++ comtest.py
@@ -7,18 +7,32 @@ ports = list(serial.tools.list_ports.comports())
 print (ports)
 for p in ports:
     print (p[1])
-    if "Arduino" in p[1]:
-	    ser=serial.Serial(port=p[0])
+    if "SERIAL" in p[1]:
+	    ser = serial.Serial(port=p[0])
     else :
 	    print ("No Arduino Device was found connected to the computer")
 #ser=serial.Serial(port='COM4')
-song1 = ['1','1','5','5','6','6','5','5','4','4','3','3','2','2','1','1']
-song2 = ['1','2','3','1','1','2','3','1','3','4','5','3','4','5']
+song1 = ['songname1','1','1','5','5','6','6','5','5','4','4','3','3','2','2','1','1']
+song2 = ['songname2','1','2','3','1','1','2','3','1','3','4','5','3','4','5']
 
 f = open('mysongs.csv', 'r')
 data = f.read()
 rows = data.split('\n')
-print(rows[0:5])
+songs= []
+for row in rows:
+    song=row.split(',')
+    songs.append(song)
+
+print(len(rows))
+print(rows[0])
+print(rows[23])
+print(rows[22])
+print(rows[21])
+print(rows[len(rows)-2])
+print(rows[1:2])
+print(rows[0:len(rows)-1])
+print(rows[0:])
+
 row=rows[0]
 #song1 = row.split(',')
 
@@ -39,11 +53,14 @@ print (n)
 def run():
     action = "aaa"
     while action != "q":
-        print ('select which tone do you want to play ? 1,2 q and others for quit')
+        print ('select which  do you want to play ? 1,2 q and others for quit')
         action = input("> ")
         if action == "1":
-            print("song name is:I don't know it is in the head of song")
-            for notes in song1:
+            print("please select the number of the song from 1 to ...")
+            print(len(rows)-1)
+            song_number = input(">>")
+            song = songs[int(song_number)-1]
+            for notes in song:
                 ser.write(notes.encode())
                 print ("send:"+notes)
                 time.sleep(1)
